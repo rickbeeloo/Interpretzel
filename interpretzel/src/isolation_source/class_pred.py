c3869215288c20ff297a6848ff0852e3d166e8fb
rickbeeloo <r.beeloo@outlook.com>
2024-06-01 19:54:33 +0200

    add % cut-off

--- interpretzel/src/isolation_source/class_pred.py
+++ interpretzel/src/isolation_source/class_pred.py
@@ -8,6 +8,7 @@ import numpy as np
 
 SYSTEM_PROMPT = """You are Interpretzel, an expert free-text classifier. Whenever you receive biological sample metada and a 
 category you carefully analyze whether the metadata fits the category and reply with a simple YES or NO. 
+You focus on where the sample is taken from and ignore the rest regardless of whether it fits a category.
 You are very objective, you do not make assumptions about the data, neither do you have any bias. 
 However, you do realize you are standardizing free text fields that might have tiny spelling mistakes which you try to interpret.
 """
@@ -24,6 +25,7 @@ class LLMPred:
         )
         self.verbose = verbose 
         self.max_tokens = max_tokens
+        print(f"Verbose: {self.verbose}")
     
     def _read_queries(self, file_path):
         unique_queries = set()
@@ -43,7 +45,7 @@ class LLMPred:
             return desc 
         
     async def _get_prompt(self, query, cat):
-        return f"""You are an expert in categorizing environmental data. Given the following metadata description, determine if it belongs to the category specified. Respond only with "YES" or "NO".
+        return f"""You are an expert in categorizing biological samples. Given the following metadata description, determine if the sample belongs to the category specified. Respond only with "YES" or "NO".
 
 Metadata: "{query}"
 Category: "{cat}"
@@ -58,14 +60,14 @@ The metadata is clearly a child of the category: [YES or NO]?
             for answer, prob in top.items():
                 # Log prob to linear prob
                 answers[answer.lower()] = np.round(np.exp(prob)*100,2)   
+        if self.verbose:
+            print(answers)
         # Check how much  more likely one is over the other
         yes_prob =  answers.get("yes", 0)
         no_prob = answers.get("no", 0)
         return {"yes":yes_prob, "no":no_prob}
 
     async def _prompt_llm(self, prompt):
-        print("model: ", self.model_name)
-        print("prompt: ", prompt)
         completion = await self.client.chat.completions.create(
             model = self.model_name,
             messages=[
@@ -84,19 +86,20 @@ The metadata is clearly a child of the category: [YES or NO]?
                 class_str = ", ".join(pred_list)
                 out.write(f"{query}\t{class_str}\n")
         
-    async def predict(self, query_file, class_file, output_file):
+    async def predict(self, query_file, class_file, output_file, cut_off=99.9):
         predictions = defaultdict(list)
         queries = self._read_queries(query_file)
         class_json = self._read_json_descriptions(class_file)
-        for query in queries:
-             if self.verbose: print(Fore.MAGENTA + "query: " + query)
+        total = len(queries)
+        for (i, query) in enumerate(queries):
+             if self.verbose: 
+                 print(Fore.MAGENTA + f"({i}/{total}): " + query + Style.RESET_ALL) 
              for class_name, desc in class_json.items():
                 class_name = class_name.strip().lstrip("#")
                 prompt = await self._get_prompt(query, class_name)
                 yes_no_prob = await self._prompt_llm(prompt)
-                print(f"Cat: {class_name}\n{yes_no_prob}\n")
-                if yes_no_prob["yes"] > 99:
-                    if self.verbose: print(Fore.GREEN + f"\t{class_name}: {yes_no_prob["yes"]}%")
+                if yes_no_prob["yes"] > cut_off:
+                    if self.verbose: print(Fore.GREEN + f"\t{class_name}: {yes_no_prob['yes']}%" + Style.RESET_ALL)
                     predictions[query].append(class_name)
         self._write_output(predictions, output_file)
     
