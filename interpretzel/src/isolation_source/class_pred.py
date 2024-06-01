29b9fd781c58e5961f01f22880bc5f93c322b0f1
rickbeeloo <r.beeloo@outlook.com>
2024-06-01 22:13:29 +0200

    Use tsv reader

--- interpretzel/src/isolation_source/class_pred.py
+++ interpretzel/src/isolation_source/class_pred.py
@@ -5,6 +5,9 @@ from collections import defaultdict
 import json 
 from colorama import Fore, Style
 import numpy as np 
+import time
+import csv
+from transformers import AutoTokenizer
 
 SYSTEM_PROMPT = """You are Interpretzel, an expert free-text classifier. Whenever you receive biological sample metada and a 
 category you carefully analyze whether the metadata fits the category and reply with a simple YES or NO. 
@@ -29,7 +32,19 @@ class LLMPred:
             print(f"Model: {self.model_name}")
             print(f"Max tokens: {self.max_tokens}")
             print(f"Client url: {base_url}")
-    
+
+    def _get_meaningless_token_bias(self):
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        bias_dict = dict()
+        for exclude in [" ", ".", "\n", "\n\n"]:
+            token_id = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(exclude))
+            bias_dict[token_id] = -100
+        if self.verbose.count("v") >= 2:
+            print("Bias tokens added")
+            for token, bias in bias_dict:
+                print(f"{token} => {bias}")
+        return bias_dict
+
     def _read_queries(self, file_path):
         unique_queries = set()
         total = 0
@@ -41,19 +56,23 @@ class LLMPred:
             print('[WARNING] We only kept unique items in your query file')
         return unique_queries
 
-    def _read_json_descriptions(self, desc_json):
-        with open(desc_json) as in_file:
-            desc = json.load(in_file)
-            print(f'[INFO] Loaded {len(desc)} descriptons')
-            return desc 
+    def _read_class_info(self, file_path):
+        class_info = dict()
+        with open(file_path, "r") as in_file:
+            reader = csv.DictReader(in_file, delimiter = "\t")
+            for row in reader:
+                class_info[row["class_name"]] = row.get("desc", "") # Description can be empty
+        return class_info
         
-    async def _get_prompt(self, query, cat):
+    async def _get_prompt(self, query, cat, desc):
         return f"""You are an expert in categorizing biological samples. Given the following metadata description, determine if the sample belongs to the category specified. Respond only with "YES" or "NO".
 
 Metadata: "{query}"
 Category: "{cat}"
+Category description: "{desc}"
 
-The metadata is clearly a child of the category: [YES or NO]?
+
+The metadata is clearly a child of the category?
 """ 
 
     async def process_reply(self, reply):
@@ -63,14 +82,14 @@ The metadata is clearly a child of the category: [YES or NO]?
             for answer, prob in top.items():
                 # Log prob to linear prob
                 answers[answer.lower()] = np.round(np.exp(prob)*100,2)   
-        if self.verbose.count('v') >=2:
-            print(answers)
         # Check how much  more likely one is over the other
         yes_prob =  answers.get("yes", 0)
         no_prob = answers.get("no", 0)
+        if self.verbose.count('v') >=2:
+            print(answers)
         return {"yes":yes_prob, "no":no_prob}
 
-    async def _prompt_llm(self, prompt):
+    async def _prompt_llm(self, prompt, bias):
         completion = await self.client.chat.completions.create(
             model = self.model_name,
             messages=[
@@ -79,31 +98,59 @@ The metadata is clearly a child of the category: [YES or NO]?
                 ],
             logprobs=True, 
             top_logprobs=2,
-            max_tokens=2
+            max_tokens=2, 
+            logit_bias=bias
+           # logit_bias={220:-100, 198:-100, 271:-100}
         )
         return await self.process_reply(completion)
 
     def _write_output(self, preds, output_file):
         with open(output_file, "w") as out:
             for query, pred_list in preds.items():
+                query = query.replace("\t", " ") # Make sure to not mess us tab delim
                 class_str = ", ".join(pred_list)
                 out.write(f"{query}\t{class_str}\n")
+
+    async def single_prediction(self, query, class_name, desc, bias, cut_off):
+        class_name = class_name.strip().lstrip("#")
+        prompt = await self._get_prompt(query, class_name, desc)
+        yes_no_prob = await self._prompt_llm(prompt, bias)
+
+        if self.verbose.count("v") >= 2:
+            print(f"{class_name}: {yes_no_prob}")
+
+        if yes_no_prob["yes"] > cut_off:    
+            if self.verbose.count("v") >=1: 
+                print(Fore.GREEN + f"\t{class_name}: {yes_no_prob['yes']}%" + Style.RESET_ALL)
+            else:
+                print(Fore.RED + f"\t{class_name}: {yes_no_prob['no']}%" + Style.RESET_ALL)
+            return True, yes_no_prob['yes']
+        return False, 0
+            
+
+    async def run_predictions(self, queries, class_json, bias, cut_off):
+        total = len(quit)
+        predictions = defaultdict(list)
+        for (i, query) in enumerate(queries):
+            s = time.time()
+            
+            if self.verbose.count("v") >=1 :
+                print(Fore.MAGENTA + f"({i}/{total}): " + query + Style.RESET_ALL) 
+
+            for class_name, desc in class_json.items():
+                passed, prob = self.single_prediction(query, class_name, desc, bias, cut_off)
+                if passed:
+                    predictions[query].append(class_name + f"[{prob}]")
+
+            took = round(time.time() - s, 2)
+            if self.verbose.count("v") >=1: print(f"Took: {took} seconds.")
+        return predictions
+
         
     async def predict(self, query_file, class_file, output_file, cut_off=99.9):
-        predictions = defaultdict(list)
         queries = self._read_queries(query_file)
-        class_json = self._read_json_descriptions(class_file)
-        total = len(queries)
-        for (i, query) in enumerate(queries):
-             if self.verbose.count("v") >=1 :
-                 print(Fore.MAGENTA + f"({i}/{total}): " + query + Style.RESET_ALL) 
-             for class_name, desc in class_json.items():
-                class_name = class_name.strip().lstrip("#")
-                prompt = await self._get_prompt(query, class_name)
-                yes_no_prob = await self._prompt_llm(prompt)
-                if yes_no_prob["yes"] > cut_off:
-                    if self.verbose.count("v") >=1: 
-                        print(Fore.GREEN + f"\t{class_name}: {yes_no_prob['yes']}%" + Style.RESET_ALL)
-                    predictions[query].append(class_name)
+        class_json = self._read_class_info(class_file)
+        bias = self._get_meaningless_token_bias()
+        predictions = await self.run_predictions(queries, class_json, bias, cut_off)
         self._write_output(predictions, output_file)
     
